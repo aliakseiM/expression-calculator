5889c833b1e4d22362e499ccf37c38f41bb7272c
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2020-03-22 23:38:36 +0300

    feat: finally complete thisssssss

--- src/index.js
+++ src/index.js
@@ -1,68 +1,64 @@
 function expressionCalculator(expr) {
-    expr = expr.replace(/ /g, '');
-    console.log("in:",expr);
-
-// check for brackets
-    if (expr.includes("(") && expr.includes(")")) {
-        let pStart = expr.indexOf("(");
-        let pEnd = expr.lastIndexOf(")");
-        let p = expr.slice(pStart+1, pEnd);
-        let pUpdate = expressionCalculator(p);
-        expr = expr.split("");
-        expr.splice(pStart, pEnd-pStart+1, pUpdate);
-    } else if (expr.includes("(") || expr.includes(")")) {
-        throw new Error("ExpressionError: Brackets must be paired");
-    } else {
-        expr = expr.split("");
+    const operators = {
+        '+': (y, x) => +x + +y,
+        '-': (y, x) => x - y,
+        '*': (y, x) => x * y,
+        '/': (y, x) => {
+            if (y == 0) {
+                throw Error("TypeError: Division by zero.");
+            } else {
+                return x / y;
+            }
+        },
+    };
+    const priority = {
+        '+': 0,
+        '-': 0,
+        '*': 1,
+        '/': 1,
+    };
+    let stack = [], digits = [];
+    let exprArr = expr.replace(/\s+/g, '').match(/[^-+*/()]+|[^]/g);
+    function count() {
+        let lastOperation = stack.pop();
+        digits.push(operators[lastOperation](digits.pop(), digits.pop()));
     }
-
-
-    //expr = expr.split("");
-    expr.push("#");
-    let stackDigits = [];
-    let newExpr = [];
-    for (let i = 0; i < expr.length; i++) {
-        if (!isNaN(expr[i])) {
-            stackDigits.push(expr[i]);
-        } else {
-            newExpr.push(sumStack(stackDigits), expr[i]);
-            stackDigits = [];
+    function checkBrackets(exprArr) {
+        const open = exprArr.filter(item => item ==='(');
+        const close = exprArr.filter(item => item ===')');
+        if (open.length !== close.length) {
+            throw Error("ExpressionError: Brackets must be paired");
         }
     }
-    newExpr.pop();
-
-    for (let i = 0; i < newExpr.length; i++) {
-        switch (newExpr[i]) {
-            case "*" : newExpr.splice(i-1, 3, newExpr[i-1]*newExpr[i+1]);
-            i = 0;
-                break;
-            case "/" : newExpr.splice(i-1, 3, newExpr[i-1]/newExpr[i+1]);
-            i = 0;
-                break;
+    function evaluate(exprArr) {
+        checkBrackets(exprArr);
+        exprArr.forEach(element => {
+            if(/\d/.test(element)) {
+                digits.push(element);
+            } else if (priority[stack[stack.length - 1]] < priority[element]) {
+                stack.push(element);
+            } else if (element === '(') {
+                stack.push(element);
+            } else if (element === ')') {
+                while (stack[stack.length-1] !== '(') {
+                    count();
+                }
+                stack.pop();
+            } else {
+                while (priority[element] <= priority[stack[stack.length - 1]]) {
+                    count();
+                }
+                stack.push(element);
+            }
+        });
+        while (stack.length > 0) {
+            count();
         }
+        return digits.pop();
     }
-    for (let i = 0; i < newExpr.length; i++) {
-        switch (newExpr[i]) {
-            case "+" : newExpr.splice(i-1, 3, Number.parseFloat(newExpr[i-1])+Number.parseFloat(newExpr[i+1]));
-            i = 0;
-                break;
-            case "-" : newExpr.splice(i-1, 3, Number.parseFloat(newExpr[i-1])-Number.parseFloat(newExpr[i+1]));
-                i = 0;
-                break;
-        }
-    }
-    console.log("before out:", newExpr[0])
-    return  newExpr[0];
-}
-
-function sumStack(st) {
-    let res = '';
-    for (let i = 0; i < st.length; i++) {
-        res += st[i];
-    }
-    return res;
+    return evaluate(exprArr);
 }
 
 module.exports = {
     expressionCalculator
-}
+};
